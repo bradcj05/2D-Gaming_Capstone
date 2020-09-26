9c0710105ede3d8468fc301b8680a14964182ee4
Harris-PFW <70552746+Harris-PFW@users.noreply.github.com>
2020-09-25 21:25:44 -0400

    Add files via upload
    Created a way for the player to control their ship in game. Will probably require refinement.

---
+++ Code/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+     public float moveSpeed = 5f;
+     public Rigidbody2D rig;
+     Vector2 movement;
+
+    // Update is called once per frame
+    //Input
+    void Update()
+    {
+          movement.x = Input.GetAxisRaw("Horizontal");
+          movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+     //Movement
+     void FixedUpdate()
+     {
+          rig.MovePosition(rig.position + movement * moveSpeed * Time.fixedDeltaTime);
+     }
+}
