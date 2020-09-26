452995940716e36b82329b48d56499fc3d926124
Harris-PFW <70552746+Harris-PFW@users.noreply.github.com>
2020-09-26 13:10:09 -0400

    Update Bullet.cs

--- Code/Bullet.cs
+++ Code/Bullet.cs
@@ -5,21 +5,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
      public int damageValue;
-     public GameObject impact;
 
      //TODO: Update to provide bullet functionality
      void OnTriggerEnter2D(Collider2D collision)
      {
-          //collision.gameObject
           Debug.Log(collision.name);
-          Enemy e = collision.GetComponent<Enemy>();
-          if(e != null)
-          {
-               //Add function for an enemy to take damage
-               //e.TakeDamage(damageValue);
-          }
-
-          Instantiate(impact, transform.position, transform.rotation);
+          //TODO add bullet damage and hit effect
 
           Destroy(gameObject);
      }
