09bb9a28731b9c6b68a7a7674d416726163c4fa1
Harris-PFW <70552746+Harris-PFW@users.noreply.github.com>
2020-09-26 10:10:33 -0400

    Add files via upload

---
+++ Code/Bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+     //TODO: Update to provide bullet functionality
+     void OnCollisionEnter2D(Collision2D collision)
+     {
+          //collision.gameObject
+          Destroy(gameObject);
+     }
+
+}
---
+++ Code/PlayerGunFire.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGunFire : MonoBehaviour
+{
+
+     public Transform gun;
+     public GameObject shellType;
+     public float speed = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+          if (Input.GetButtonDown("Fire1"))
+          {
+               Fire();
+          }
+    }
+
+     public void Fire()
+     {
+          GameObject bullet = Instantiate(shellType, gun.position, gun.rotation);
+          Rigidbody2D rig = bullet.GetComponent<Rigidbody2D>();
+          rig.AddForce(gun.up * speed, ForceMode2D.Impulse);
+     }
+}
