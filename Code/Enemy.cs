733515d7c91593ca32a91e77c8c27f56b38dba7d
Harris-PFW <70552746+Harris-PFW@users.noreply.github.com>
2020-09-26 22:32:28 -0400

    Add files via upload

---
+++ Code/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+     public int health;
+     public int defense;
+     //Add death animation
+
+     public void TakeDamage (int damage)
+     {
+          health -= (damage - defense);
+          if(health <= 0)
+          {
+               Die();
+          }
+     }
+
+     void Die()
+     {
+          //Play death animation
+          Destroy(gameObject);
+     }
+}
