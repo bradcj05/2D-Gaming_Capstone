00a11feb4b36aadcbb9e48bc558c48ce99d29d5d
Harris-PFW <70552746+Harris-PFW@users.noreply.github.com>
2020-10-03 21:43:59 -0400

    Update Player.cs

--- Code/Player.cs
+++ Code/Player.cs
@@ -9,6 +9,16 @@ public class Player : MonoBehaviour
      public Rigidbody2D rig;
      Vector2 movement;
 
+     public int health;
+     public int defense;
+     public HealthBar hb;
+     //Add death animation
+
+
+    void Start()
+     {
+          hb.SetMax(health);
+     }
     // Update is called once per frame
     //Input
     void Update()
@@ -21,5 +31,34 @@ public class Player : MonoBehaviour
      void FixedUpdate()
      {
           rig.MovePosition(rig.position + movement * moveSpeed * Time.fixedDeltaTime);
+     }
+
+     //Take damage
+     public void TakeDamage(int damage)
+     {
+          if ((damage - defense) > 0)
+          {
+               health -= (damage - defense);
+               if(health < 0)
+               {
+                    health = 0;
+               }
+               hb.SetHealth(health);
+          }
+
+          if(health <= 0)
+          {
+               Die();
+          }
+     }
+
+     //Player Death
+     void Die()
+     {
+          //Play death animation
+          Destroy(gameObject);
+
+          //Load game over screen / swap to another plane
+
      }
 }
