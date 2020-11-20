769c4c3b953f89b1bafb69cc384ce46154a93db5
Hong Phuc Nguyen <nguyhp02@pfw.edu>
2020-11-19 23:56:16 -0500

    Re...build.... Libra...ry?

--- Code/CapstoneDev/Library/PackageCache/com.unity.timeline@1.2.16/Editor/Utilities/TimeReferenceUtility.cs
+++ Code/CapstoneDev/Library/PackageCache/com.unity.timeline@1.2.16/Editor/Utilities/TimeReferenceUtility.cs
@@ -1,3 +1,57 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c7d46a4b52ec43e540b33faea727cb8b513dcca788ec213b73a3f8bd069542a1
-size 2016
+using UnityEngine.Timeline;
+
+namespace UnityEditor.Timeline
+{
+    static class TimeReferenceUtility
+    {
+        static WindowState state { get { return TimelineWindow.instance.state; } }
+
+        public static double SnapToFrame(double time)
+        {
+            if (state.timeReferenceMode == TimeReferenceMode.Global)
+            {
+                time = state.editSequence.ToGlobalTime(time);
+                time = TimeUtility.RoundToFrame(time, state.referenceSequence.frameRate);
+                return state.editSequence.ToLocalTime(time);
+            }
+
+            return TimeUtility.RoundToFrame(time, state.referenceSequence.frameRate);
+        }
+
+        public static string ToTimeString(double time, string format = "F2")
+        {
+            if (state.timeReferenceMode == TimeReferenceMode.Global)
+                time = state.editSequence.ToGlobalTime(time);
+
+            return state.editSequence.viewModel.timeInFrames ?
+                TimeUtility.TimeAsFrames(time, state.referenceSequence.frameRate, format) :
+                TimeUtility.TimeAsTimeCode(time, state.referenceSequence.frameRate, format);
+        }
+
+        public static double FromTimeString(string timeString)
+        {
+            double newTime;
+
+            if (state.timeInFrames)
+            {
+                double newFrameDouble;
+                if (double.TryParse(timeString, out newFrameDouble))
+                    newTime = TimeUtility.FromFrames(newFrameDouble, state.referenceSequence.frameRate);
+                else
+                    newTime = state.editSequence.time;
+            }
+            else
+            {
+                newTime = TimeUtility.ParseTimeCode(timeString, state.referenceSequence.frameRate, -1);
+            }
+
+            if (newTime >= 0.0)
+            {
+                return state.timeReferenceMode == TimeReferenceMode.Global ?
+                    state.editSequence.ToLocalTime(newTime) : newTime;
+            }
+
+            return state.editSequence.time;
+        }
+    }
+}
