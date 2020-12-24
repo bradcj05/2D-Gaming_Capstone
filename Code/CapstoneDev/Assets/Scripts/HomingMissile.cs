47e811a75bebd192201fe1640613bdfe3bbef81e
Harris-PFW <70552746+Harris-PFW@users.noreply.github.com>
2020-12-24 12:19:10 -0500

    Airos HM changes
    Fleched out the HomingMissiles script for the Airos boss fight. Still need to actually apply it and  make a proper interface. Committing what I have now so I have a version to go back to in case something breaks and I can't fix it.

--- Code/CapstoneDev/Assets/Scripts/HomingMissile.cs
+++ Code/CapstoneDev/Assets/Scripts/HomingMissile.cs
@@ -2,15 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//TODO fix so that it inherits from EnemyBullet
+//TODO fix so that it inherits from EnemyBullet or another interface
+[RequireComponent(typeof(Rigidbody2D))]
 public class HomingMissile : MonoBehaviour
 {
      public float power;
      public float speed;
+     public float rotateSpeed = 10f;
+     float rotateAmount;
      public float penetration;
      public float deterioration; //ratio/second
+
      public Transform target;
      private Rigidbody2D rb;
+     //TODO Implement timer
+     float timer;
+     public float rotationTime = 5f;
 
      //For Matt's explosion animation
      public GameObject explosion;
@@ -18,11 +25,26 @@ public class HomingMissile : MonoBehaviour
      void Start()
      {
           rb = GetComponent<Rigidbody2D>();
+          target = GameObject.FindGameObjectWithTag("Player").transform;
+          timer = 0f;
      }
 
+     //Handles the physics and math for the homing missile
+     //TODO test
      void FixedUpdate()
      {
+          if (timer < rotationTime)
+          {
+               Vector2 direction = (Vector2)target.position - rb.position;
+               direction.Normalize();
+               rotateAmount = Vector3.Cross(direction, transform.up).z;
 
+               timer += Time.deltaTime;
+          }
+          else
+               rotateAmount = 0;
+          rb.angularVelocity = -rotateAmount * rotateSpeed;
+          rb.velocity = transform.up * speed;
      }
 
      //TODO: Update to provide bullet functionality
