63f873fd1d9ad420138aa9106c7c336b9d8c0511
Ricky <ochor01@pfw.edu>
2021-01-17 17:17:39 -0500

    Creating individuals cards to represent store items
    I will have a demo talk about the details soon.
    
    Worked more on design, implementing, and bug fixing.
    
    After the rebase it screwed with some sizing issues.

---
+++ Code/CapstoneDev/Assets/Scripts/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName ="Cards/PlayerPlanes")]
+//This will be a template frr the data to store.
+
+public class Card : ScriptableObject{
+    public new string name;
+    public string cost;
+    public Sprite artwork;
+
+
+
+}
---
+++ Code/CapstoneDev/Assets/Scripts/cardDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class cardDisplay : MonoBehaviour
+{
+    public GameObject attributes;
+  
+
+    public Card cards;
+
+    public Image artwork;
+    public Text nameText;
+    public Text costText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nameText.text = cards.name;
+        costText.text = cards.cost;
+        artwork.sprite = cards.artwork;
+    }
+
+   
+
+
+}
