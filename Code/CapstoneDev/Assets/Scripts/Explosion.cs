7298c87dda89ce02ba7f00b38e74df789fca4475
Hong Phuc Nguyen <nguyhp02@pfw.edu>
2021-01-21 10:13:09 -0500

    Terrod params edit
    Small param changes on Terrod: Increased small turret fire rate and decreased flak explosion power. Also made the specified explosion power the maximum possible damage from the explosion. This is done to avoid frustration upon direct hits.

--- Code/CapstoneDev/Assets/Scripts/Explosion.cs
+++ Code/CapstoneDev/Assets/Scripts/Explosion.cs
@@ -19,8 +19,6 @@ public class Explosion : MonoBehaviour
         Collider2D[] targets = Physics2D.OverlapCircleAll(transform.position, radius, LayerMask.GetMask("Player"));
         foreach (Collider2D hit in targets)
         {
-            //Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
-            //rb.AddExplosionForce(power, explosionPos, radius, 0f, ForceMode.Impulse);
             Vector3 distanceVec = hit.transform.position - transform.position;
             float distance = distanceVec.magnitude;
             if (distance > 0)
@@ -42,7 +40,7 @@ public class Explosion : MonoBehaviour
                 Destructible p = hit.GetComponent<Destructible>();
                 if (p != null)
                 {
-                    p.TakeDamage(effectivePower - p.defense);
+                    p.TakeDamage(Mathf.Max(power,effectivePower) - p.defense);
                 }
                 // Knockback
                 Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
@@ -54,24 +52,4 @@ public class Explosion : MonoBehaviour
             }
         }
     }
-
-    /*//Damages the player if they collide with the explosion
-    void OnTriggerEnter2D(Collider2D collision)
-    {
-
-         Debug.Log(collision.name);
-
-         Player p = collision.GetComponent<Player>();
-         float distance = (source.transform.position - p.transform.position).magnitude; //Change
-         if(distance <= radius)
-         {
-              float effectivePower = power / Math.Pow(distance, 2);
-              float effectiveKnockback = forceThreshold * Math.Pow(radius / distance, 2);
-              if(p != null)
-                   p.TakeDamage(effectivePower);
-              Knockback(collision.gameObject, effectiveKnockback); //Change
-         }
-
-    }
-    */
 }
