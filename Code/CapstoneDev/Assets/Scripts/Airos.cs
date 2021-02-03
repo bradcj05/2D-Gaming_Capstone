34e15b2f48418e698bea37a4a3fe4c31b6267029
Connor Bradt <connor.bradt@gmail.com>
2021-02-03 15:44:20 -0500

    Path randomizer

---
+++ Code/CapstoneDev/Assets/Scripts/Airos.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airos : Enemy /// Always include "Enemy" and "Die()" function
+{
+    //Movement Variables
+    
+    public Rigidbody2D rig;
+    public Rigidbody2D turretL1;
+    public Rigidbody2D turretR2;
+    //Vector2 movement;
+
+   // public float moveSpeed = 5f;
+    public float rotateSpeed = 0.01f;
+    protected float rotateAmount; //public for better testing
+    protected Transform target;
+
+    [SerializeField]
+    private Transform[] Path;
+    private int routeToGo;
+    private float tParam;
+    private Vector2 AirPosition;
+    public float speed;
+    private bool coroutineAllowed;
+
+   
+    //Path randomizer variables and selector
+    double p1 = 0.3;
+    double p2 = 0.4;
+    double p3 = 0.3;
+
+    double Rp1 = 0.4;
+    double Rp2 = 0.6;
+
+    double select;
+    int start = 1;  /// keep an eye on the start position switching
+    int path;
+
+
+    new void Start()
+    {
+
+        routeToGo = 0;
+        tParam = 0f;
+        // speed = 0.5f;
+        coroutineAllowed = true;
+        Randomize(); // random start path
+
+        try
+        {
+            target = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+        catch (System.NullReferenceException e)
+        {
+            Debug.Log(e);
+            target = null;
+        }
+    }
+
+    new void Update() //// check on this HERE!!!!!!!!
+    {
+        if (coroutineAllowed)
+            StartCoroutine(GoByTheRoute(routeToGo));
+    }           //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    void FixedUpdate()
+    {
+        if (target != null)
+        {
+            Vector2 direction = (Vector2)target.position - rig.position;
+            direction.Normalize();
+            if (Vector3.Dot(direction, -transform.up) <= 0)
+            {
+                rotateAmount = 1f;
+            }
+            else
+            {
+                rotateAmount = Vector3.Cross(direction, -transform.up).z;
+            }
+            float curRot = transform.rotation.eulerAngles.z;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, curRot - rotateSpeed * rotateAmount));
+           // rig.velocity = -transform.up * moveSpeed;
+        }
+        else
+        {
+            //Try to find the next player plane when it spawns
+            try
+            {
+                target = GameObject.FindGameObjectWithTag("Player").transform;
+            }
+            catch (System.NullReferenceException e)
+            {
+                Debug.Log(e);
+                target = null;
+            }
+        }
+    }
+
+    private IEnumerator GoByTheRoute(int routeNumber)
+    {
+
+        coroutineAllowed = false;
+
+        //Path --> route --> position point.  what this code is doing
+
+        Vector2 p0 = Path[path].GetChild(routeNumber).GetChild(0).position;  
+        Vector2 p1 = Path[path].GetChild(routeNumber).GetChild(1).position;
+        Vector2 p2 = Path[path].GetChild(routeNumber).GetChild(2).position;
+        Vector2 p3 = Path[path].GetChild(routeNumber).GetChild(3).position;
+
+        while (tParam < 1)
+        {
+            tParam += Time.deltaTime * speed;   /// requires some form of speed variable
+
+            AirPosition = Mathf.Pow(1 - tParam, 3) * p0 +
+                       3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 +
+                       3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 +
+                        Mathf.Pow(tParam, 3) * p3;
+
+            transform.position = AirPosition;
+            yield return new WaitForEndOfFrame();
+        }
+
+        tParam = 0;
+
+        routeToGo += 1;
+
+        if (routeToGo > Path[path].childCount - 1){ ///was routes.Length still counts the number of routes
+            routeToGo = 0;
+            Randomize();
+        }
+
+        coroutineAllowed = true;
+
+    }
+
+
+
+    public void Randomize()   // Path randomizer
+    {
+        select = Random.Range(0.0f, 1.0f);
+        if (start == 1)
+        {
+
+            if (select <= p1)
+            {
+                path = 0;
+
+            }
+            else if (select < p1 + p2)
+            {
+                path = 1;
+                start = 2;
+
+            }
+            else
+            {
+                path = 2;
+                start = 2;
+            }
+        }
+        else
+        {
+            if (select <= Rp1)
+            {
+                path = 3;
+
+            }
+            else
+            {
+                path = 4;
+                start = 1;
+            }
+        }
+        
+    }
+}
+
+
