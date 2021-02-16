8dc2de0b7762b404e24ac55ae235f50b8c032d04
Hong Phuc Nguyen <nguyhp02@pfw.edu>
2021-02-16 15:47:26 -0500

    Organized scripts into folders

--- Code/CapstoneDev/Assets/Scripts/Database/Card.cs
+++ Code/CapstoneDev/Assets/Scripts/Database/Card.cs
@@ -3,39 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[CreateAssetMenu(fileName = "New Card", menuName = "Cards/PlayerPlanes")]
+[CreateAssetMenu(fileName = "New Card", menuName ="Cards/PlayerPlanes")]
 //This will be a template frr the data to store.
 
-public class Card : ScriptableObject
-{
-    public int cardType; // 1 for plane, 2 for gun, 3 for shell
-    public GameObject obj;
-    public int unlock;
-    public new string name;
-    public int cost;
-    public Sprite artwork;
+public class Card : ScriptableObject{
+     public int cardType; // 1 for plane, 2 for gun, 3 for shell
+     public GameObject obj;
+     public int unlock;
+     public new string name;
+     public int cost;
+     public Sprite artwork;
+     public GameObject hangarView; //For planes only
 
-    // Plane values
-    public string speed;
-    public string defense;
-    public string health;
-    //Don't know if we'll need this
-    public string[] gunSlotClass;
-    public string[] gunSlotSpecs;
+     // Plane values
+     public string speed;
+     public string defense;
+     public string health;
+     //Don't know if we'll need this
+     public string[] gunSlotClass;
+     public string[] gunSlotSpecs;
 
-    // Gun values
-    public string reloadTime;
-    public string powerBuff;
-    public string speedBuff;
+     // Gun values
+     public string reloadTime;
+     public string powerBuff;
+     public string speedBuff;
 
-    // Shell values
-    public string power;
-    public string shellSpeed;
-    public string penetration;
-    public string deterioration;
+     // Shell values
+     public string power;
+     public string shellSpeed;
+     public string penetration;
+     public string deterioration;
 
-    // Only needed for guns and shells
-    //Don't know if we'll need this
-    public string type;
-    public string classOf;
+     // Only needed for guns and shells
+     //Don't know if we'll need this
+     public string type;
+     public string classOf;
 }
