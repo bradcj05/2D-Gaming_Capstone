9a86fdfa2f0180a25493a420d7ebe04bed6ce0fd
Hong Phuc Nguyen <nguyhp02@pfw.edu>
2021-02-17 15:00:51 -0500

    Make Ju-5's gun into a prefab
    - Create prefab for Ju-5's new machinegun (instead of Pancake's gun)
    - Fixed the healthbars for Terrod's flaks AGAIN.
    - Started gun slot scripts

---
+++ Code/CapstoneDev/Assets/Scripts/Database/GunSlot.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunSlot : MonoBehaviour
+{
+    // Gun classification params
+    [System.Serializable]
+    public enum Category
+    {
+        Enemy = 0,
+        Primary = 1,
+        Secondary = 2
+    }
+
+    [System.Serializable]
+    public enum Grade
+    {
+        Light = 0,
+        Medium = 1,
+        Heavy = 2,
+    }
+
+    [System.Serializable]
+    public enum Type
+    {
+        RPM = 0,
+        Damage = 1,
+        Penetration = 2
+    }
+
+    public Type type;
+    public Grade grade;
+    public Category category;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
