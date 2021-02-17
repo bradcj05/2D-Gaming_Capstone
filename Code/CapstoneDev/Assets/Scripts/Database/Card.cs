837509e435d90f9dbbf51ed154c2427c89b8df16
Hong Phuc Nguyen <nguyhp02@pfw.edu>
2021-02-17 17:13:53 -0500

    Start making cards automatic + gun classification
    NOT YET FUNCTIONAL!!!

--- Code/CapstoneDev/Assets/Scripts/Database/Card.cs
+++ Code/CapstoneDev/Assets/Scripts/Database/Card.cs
@@ -8,12 +8,20 @@ using UnityEngine.UI;
 
 public class Card : ScriptableObject
 {
-    public int cardType; // 1 for plane, 2 for gun, 3 for shell
+    [System.Serializable]
+    public enum CardType
+    {
+        Plane = 1,
+        Gun = 2,
+        Shell = 3
+    }
+    public CardType cardType; // 1 for plane, 2 for gun, 3 for shell
     public GameObject obj;
-    public int unlock;
+    public int unlockLevel = 1;  // The LEVEL that the equipment is unlocked in
     public new string name;
     public int cost;
     public Sprite artwork;
+    public string description;
     public GameObject hangarView; //For planes only
 
 
@@ -22,26 +30,140 @@ public class Card : ScriptableObject
      public string powerBuff;
      public string speedBuff;
     // Plane values
-    public string speed;
-    public string defense;
-    public string health;
-    //Don't know if we'll need this
-    public string[] gunSlotClass;
-    public string[] gunSlotSpecs;
+    protected string speed;
+    protected string defense;
+    protected string health;
+    protected string mass;
+
+    // Gun classification params
+    protected WeaponsClassification.Type type;
+    protected WeaponsClassification.Grade grade;
+    protected WeaponsClassification.Category category;
 
     // Gun values
-    public string reloadTime;
-    public string powerBuff;
-    public string speedBuff;
+    protected string reloadTime;
+    protected string powerBuff;
+    protected string speedBuff;
+    protected string spread;
 
     // Shell values
-    public string power;
-    public string shellSpeed;
-    public string penetration;
-    public string deterioration;
+    protected string power;
+    protected string shellSpeed;
+    protected string penetration;
+    protected string deterioration;
 
-    // Only needed for guns and shells
-    //Don't know if we'll need this
-    public string type;
-    public string classOf;
+    // Retrieve info from object
+    public void Start()
+    {
+        switch ((int)cardType)
+        {
+            case 1:
+                Player plane = obj.GetComponent<Player>();
+                Rigidbody2D planeRig = obj.GetComponent<Rigidbody2D>();
+                speed = plane.maxSpeed.ToString();
+                defense = plane.defense.ToString();
+                health = plane.health.ToString();
+                mass = planeRig.mass.ToString();
+                break;
+            case 2:
+                Gun gun = obj.GetComponent<Gun>();
+                type = gun.type;
+                grade = gun.grade;
+                category = gun.category;
+                reloadTime = gun.waitTime.ToString();
+                powerBuff = gun.powerBuff.ToString();
+                speedBuff = gun.speedBuff.ToString();
+                spread = gun.spread.ToString();
+                break;
+            case 3:
+                Bullet shell = obj.GetComponent<Bullet>();
+                type = shell.type;
+                grade = shell.grade;
+                category = shell.category;
+                power = shell.power.ToString();
+                shellSpeed = shell.speed.ToString();
+                penetration = shell.penetration.ToString();
+                deterioration = shell.deterioration.ToString();
+                break;
+            default:
+                Debug.Log("Please give this card the right card type.");
+                break;
+        }
+    }
+
+    // Getters
+    public string getSpeed()
+    {
+        return speed;
+    }
+
+    public string getDefense()
+    {
+        return defense;
+    }
+
+    public string getHealth()
+    {
+        return health;
+    }
+
+    public string getMass()
+    {
+        return mass;
+    }
+
+    public WeaponsClassification.Type getType()
+    {
+        return type;
+    }
+
+    public WeaponsClassification.Grade getGrade()
+    {
+        return grade;
+    }
+
+    public WeaponsClassification.Category getCategory()
+    {
+        return category;
+    }
+
+    public string getReloadTime()
+    {
+        return reloadTime;
+    }
+
+    public string getPowerBuff()
+    {
+        return powerBuff;
+    }
+
+    public string getSpeedBuff()
+    {
+        return speedBuff;
+    }
+
+    public string getSpread()
+    {
+        return spread;
+    }
+
+    public string getPower()
+    {
+        return power;
+    }
+
+    public string getShellSpeed()
+    {
+        return shellSpeed;
+    }
+
+    public string getPenetration()
+    {
+        return penetration;
+    }
+
+    public string getDeterioration()
+    {
+        return deterioration;
+    }
 }
