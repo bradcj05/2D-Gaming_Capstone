8f0731bb096a235d0fe7dab508923a4b198cc272
Connor Bradt <connor.bradt@gmail.com>
2021-03-10 20:42:20 -0500

    Player remains on camera

---
+++ Code/CapstoneDev/Assets/Scripts/Other Mechanics/EDGE_LORD.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EDGE_LORD : MonoBehaviour { 
+   private void Awake() {
+    PolygonCollider2D poly = GetComponent<PolygonCollider2D>();
+    if (poly == null)
+    {
+        poly = gameObject.AddComponent<PolygonCollider2D>();
+    }
+    Vector2[] points = poly.points;
+    EdgeCollider2D edge = gameObject.AddComponent<EdgeCollider2D>();
+    edge.points = points;
+    Destroy(poly);
+}
+}
