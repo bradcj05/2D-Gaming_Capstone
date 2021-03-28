fd76a4d070ab9b3505eb05d50f7793997ac318b0
Harris-PFW <70552746+Harris-PFW@users.noreply.github.com>
2021-03-27 21:56:27 -0400

    Sidebar work started, script created.

---
+++ Code/CapstoneDev/Assets/Scripts/UI and Controllers/Sidebars.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sidebars : MonoBehaviour
+{
+     public Text currentPlaneName;
+     public Image currentPlaneIcon;
+     public Text sidePlaneName1;
+     public Image sidePlaneIcon1;
+     public Text sidePlaneName2;
+     public Image sidePlaneIcon2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
