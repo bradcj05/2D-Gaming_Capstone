7778308781749d4e548b889ff24fc345f58253e8
Harris-PFW <70552746+Harris-PFW@users.noreply.github.com>
2021-04-03 15:38:26 -0400

    Narration System basic implemented

---
+++ Code/CapstoneDev/Assets/Scripts/UI and Controllers/Line.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Narration Line", menuName = "Narration/Line")]
+
+public class Line : ScriptableObject
+{
+     public Sprite portrait;
+     public string text;
+     public float trigger;
+
+     void Awake()
+     {
+        
+     }
+}
